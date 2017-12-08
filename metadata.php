50b82c3b5c15e550c14914a14dbf63dd1df274ba
hkreuter <heike@reuter-consultig.org>
2017-12-08 12:47:30 +0100

    Add metadata.php (does not yet work)

---
+++ metadata.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'cleartmp',
+    'title'       => array(
+        'de' => 'OXID-cleartmp for OXID eShop 6',
+        'en' => 'OXID-cleartmp for OXID eShop 6',
+    ),
+    'description' => array(
+        'de' => 'OXID-cleartmp for OXID eShop 6',
+        'en' => 'OXID-cleartmp for OXID eShop 6',
+    ),
+    'thumbnail'   => 'out/pictures/picture.png',
+    'version'     => '2.0.0',
+    'author'      => 'OXID Hackathon 2017',
+    'url'         => 'http://www.oxid-esales.com/',
+    'email'       => '',
+    'extend'      => array(),
+    'files'       => array(
+        'navigation' => 'ox/cleartmp/core/xxx.php'
+    ),
+    'templates'   => array(),
+    'blocks'      => array(),
+    'settings'    => array(),
+    'events'      => array(),
+);
