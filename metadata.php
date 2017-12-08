5f36b3c5c9957e5ad129f1ab14aa6cdaebb1c04f
C. Bauer <info@crs-design.eu>
2017-12-08 15:48:32 +0100

    update view

--- metadata.php
+++ metadata.php
@@ -9,27 +9,31 @@ $sMetadataVersion = '2.0';
  * Module information
  */
 $aModule = [
-	'id'          => 'cleartmp',
-	'title'       => [
-		'de' => 'OXID-cleartmp for OXID eShop 6',
-		'en' => 'OXID-cleartmp for OXID eShop 6',
-	],
-	'description' => [
-		'de' => 'OXID-cleartmp for OXID eShop 6',
-		'en' => 'OXID-cleartmp for OXID eShop 6',
-	],
-	'thumbnail'   => 'out/pictures/picture.png',
-	'version'     => '2.0.0',
-	'author'      => 'OXID Hackathon 2017',
-	'url'         => 'http://www.oxid-esales.com/',
-	'email'       => '',
-	'extend'      => [
-		\OxidEsales\Eshop\Application\Controller\Admin\NavigationController::class => \Hackathon2017\ClearTmp\Controller\NavigationController::class,
-	],
-	'templates'   => [
-		'header.tpl' => 'ox/cleartmp/views/admin/tpl/header.tpl',
-	],
-	'blocks'      => [],
-	'settings'    => [],
-	'events'      => [],
+    'id'          => 'cleartmp',
+    'title'       => [
+        'de' => 'OXID-cleartmp for OXID eShop 6',
+        'en' => 'OXID-cleartmp for OXID eShop 6',
+    ],
+    'description' => [
+        'de' => 'OXID-cleartmp for OXID eShop 6',
+        'en' => 'OXID-cleartmp for OXID eShop 6',
+    ],
+    'thumbnail'   => 'out/pictures/picture.png',
+    'version'     => '2.0.0',
+    'author'      => 'OXID Hackathon 2017',
+    'url'         => 'http://www.oxid-esales.com/',
+    'email'       => '',
+    'extend'      => [
+        \OxidEsales\Eshop\Application\Controller\Admin\NavigationController::class => \Hackathon2017\ClearTmp\Controller\NavigationController::class,
+    ],
+    'templates'   => [
+        'header.tpl' => 'ox/cleartmp/views/admin/tpl/header.tpl',
+    ],
+    'blocks'      => [
+        [
+            'template' => 'admin/header.tpl',
+            'block'    => 'admin_header_links',
+            'file'     => '/views/blocks/header__admin_header_links.tpl',
+        ],
+    ],
 ];
---
+++ views/blocks/header__admin_header_links.tpl
@@ -0,0 +1,5 @@
+[{$smarty.block.parent}]
+<li class="sep">
+    <a href="[{$oViewConf->getSelfLink()}]&cl=navigation&amp;fnc=cleartmp" id="cleartmplink" target="hiddenframe" class="rc"><b>[{ oxmultilang ident="NAVIGATION_CLEARTMP" }]</b></a>
+    <iframe name="hiddenframe" style="display:none;"></iframe>
+</li>
