5116790bfce53ae48ee9a81cb8ce4743444eeefb
hkreuter <heike@reuter-consultig.org>
2017-12-08 15:33:18 +0100

    Update test

--- Tests/Integration/Controller/NavigationControllerTest.php
+++ Tests/Integration/Controller/NavigationControllerTest.php
@@ -4,10 +4,20 @@ namespace Hackathon2017\ClearTmp\Tests\Integration\Controller;
 
 class NavigationControllerTest extends \OxidEsales\TestingLibrary\UnitTestCase
 {
-
-    public function testSomething()
+    /**
+     * Test cleaning tmp directory.
+     */
+    public function testTmpCleaning()
     {
-        $this->markTestIncomplete('TODO');
-    }
+        //preparation: fill compile dir
+        file_get_contents(\OxidEsales\eShop\Core\Registry::getConfig()->getShopUrl());
+        $this->assertTrue(is_dir(\OxidEsales\eShop\Core\Registry::getConfig()->getConfigParam('sCompileDir') . DIRECTORY_SEPARATOR . 'smarty'));
 
+        $navigation = oxNew(\OxidEsales\Eshop\Application\Controller\Admin\NavigationController::class);
+        $navigation->clearTmp();
+
+        //asserts
+        $this->assertFalse(is_dir(\OxidEsales\eShop\Core\Registry::getConfig()->getConfigParam('sCompileDir') . DIRECTORY_SEPARATOR . 'smarty'));
+        $this->assertTrue(file_exists(\OxidEsales\eShop\Core\Registry::getConfig()->getConfigParam('sCompileDir') . '.htaccess'));
+    }
 }
