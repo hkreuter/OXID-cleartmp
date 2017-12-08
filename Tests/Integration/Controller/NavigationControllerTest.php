565e8af6c363b89a96e8a60c96435d99173215ec
hkreuter <heike@reuter-consultig.org>
2017-12-08 13:58:50 +0100

    Add test

---
+++ Tests/Integration/Controller/NavigationControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hackathon2017\ClearTmp\Tests\Integration\Controller;
+
+class NavigationControllerTest extends OxidEsales\TestingLibrary\UnitTestCase
+{
+
+    public function testSomething()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+}
