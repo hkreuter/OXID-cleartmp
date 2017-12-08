ccd3fbc865a64207a4cf5bcf048242af20e7f52f
felixflu <felix.lehmann@posteo.de>
2017-12-08 13:39:58 +0100

    Codestyle: converted tabs to white spaces

--- Controller/NavigationController.php
+++ Controller/NavigationController.php
@@ -4,16 +4,16 @@ namespace Hackathon2017\ClearTmp\Controller;
 class NavigationController extends Navigation_parent
 {
     /**
-	 * Function to read the tmp-directory and delete the files
-	 */
+     * Function to read the tmp-directory and delete the files
+     */
     public function cleartmp()
     {
         $aFiles = glob('../tmp/*');
         foreach($aFiles as $file) {
-			if ($file != '.htaccess')
-			{
-				unlink($file);
-			}
-		}
+            if ($file != '.htaccess')
+            {
+                unlink($file);
+            }
+        }
     }
 }
--- core/navigation_ext.php
+++ core/navigation_ext.php
@@ -2,16 +2,16 @@
 class navigation_ext extends navigation_ext_parent
 {
     /**
-	 * Function to read the tmp-directory and delete the files
-	 */
+     * Function to read the tmp-directory and delete the files
+     */
     public function cleartmp()
     {
         $aFiles = glob('../tmp/*');
         foreach($aFiles as $file) {
-			if ($file != '.htaccess')
-			{
-				unlink($file);
-			}
-		}
+            if ($file != '.htaccess')
+            {
+                unlink($file);
+            }
+        }
     }
 }
