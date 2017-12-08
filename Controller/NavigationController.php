c491c3051c5418acd39b65aabc5629fb7160c9ac
felixflu <felix.lehmann@posteo.de>
2017-12-08 13:42:32 +0100

    Codestyle: some more formatting

--- Controller/NavigationController.php
+++ Controller/NavigationController.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace Hackathon2017\ClearTmp\Controller;
 
+/**
+ * Class NavigationController:
+ */
 class NavigationController extends Navigation_parent
 {
     /**
      * Function to read the tmp-directory and delete the files
      */
-    public function cleartmp()
+    public function clearTmp()
     {
         $aFiles = glob('../tmp/*');
         foreach($aFiles as $file) {
