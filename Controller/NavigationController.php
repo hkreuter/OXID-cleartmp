c70dda8b775faa0e6528c004f59870d0cad9746b
C. Bauer <info@crs-design.eu>
2017-12-08 13:05:47 +0100

    added controller and namespace

---
+++ Controller/NavigationController.php
@@ -0,0 +1,19 @@
+<?php
+namespace \ClearTmp\controllers;
+
+class NavigationController extends Navigation_parent
+{
+    /**
+	 * Function to read the tmp-directory and delete the files
+	 */
+    public function cleartmp()
+    {
+        $aFiles = glob('../tmp/*');
+        foreach($aFiles as $file) {
+			if ($file != '.htaccess')
+			{
+				unlink($file);
+			}
+		}
+    }
+}
--- metadata.php
+++ metadata.php
@@ -3,32 +3,34 @@
 /**
  * Metadata version
  */
-$sMetadataVersion = '1.1';
+$sMetadataVersion = '2.0';
 
 /**
  * Module information
  */
-$aModule = array(
-    'id'          => 'cleartmp',
-    'title'       => array(
-        'de' => 'OXID-cleartmp for OXID eShop 6',
-        'en' => 'OXID-cleartmp for OXID eShop 6',
-    ),
-    'description' => array(
-        'de' => 'OXID-cleartmp for OXID eShop 6',
-        'en' => 'OXID-cleartmp for OXID eShop 6',
-    ),
-    'thumbnail'   => 'out/pictures/picture.png',
-    'version'     => '2.0.0',
-    'author'      => 'OXID Hackathon 2017',
-    'url'         => 'http://www.oxid-esales.com/',
-    'email'       => '',
-    'extend'      => array(),
-    'files'       => array(
-        'navigation' => 'ox/cleartmp/core/xxx.php'
-    ),
-    'templates'   => array(),
-    'blocks'      => array(),
-    'settings'    => array(),
-    'events'      => array(),
-);
+$aModule = [
+	'id'          => 'cleartmp',
+	'title'       => [
+		'de' => 'OXID-cleartmp for OXID eShop 6',
+		'en' => 'OXID-cleartmp for OXID eShop 6',
+	],
+	'description' => [
+		'de' => 'OXID-cleartmp for OXID eShop 6',
+		'en' => 'OXID-cleartmp for OXID eShop 6',
+	],
+	'thumbnail'   => 'out/pictures/picture.png',
+	'version'     => '2.0.0',
+	'author'      => 'OXID Hackathon 2017',
+	'url'         => 'http://www.oxid-esales.com/',
+	'email'       => '',
+	'extend'      => [],
+	'files'       => [
+		OxidEsales\EshopCommunity\Application\Controller\Admin\NavigationController::class => ClearTmp\Controller\NavigationController::clas,
+	],
+	'templates'   => [
+		'header.tpl' => 'OXID-cleartmp/views/admin/tpl/header.tpl',
+	],
+	'blocks'      => [],
+	'settings'    => [],
+	'events'      => [],
+];
